f71594974ebc7e60a0d63a73aee1ea463dc05151
Christian Binay-an <christianbinayan3@gmail.com>
2026-01-06 10:51:21 -0500

    changes to authroutes profile

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -116,44 +116,35 @@ router.put('/update', async (req, res) => {
             logger.error('Email not found in the request headers');
             return res.status(400).json({ error: "Email not found in the request headers" });
         }
-
-        // Task 4: Connect to MongoDB
+        //Task 4: Connect to MongoDB
         const db = await connectToDatabase();
         const collection = db.collection("users");
-
-        // Task 5: find user credentials in database
-        const existingUser = await collection.findOne({ email })
-
-
+        //Task 5: Find user credentials
+        const existingUser = await collection.findOne({ email });
         if (!existingUser) {
-            logger.error('User not found'
-            );
+            logger.error('User not found');
             return res.status(404).json({ error: "User not found" });
         }
-
         existingUser.firstName = req.body.name;
         existingUser.updatedAt = new Date();
-
-        // Task 6: update user credentials in database
+        //Task 6: Update user credentials in DB
         const updatedUser = await collection.findOneAndUpdate(
             { email },
             { $set: existingUser },
             { returnDocument: 'after' }
         );
-        
-
-= {}}
+        //Task 7: Create JWT authentication with user._id as payload using secret key from .env file
+        const payload = {
             user: {
-                id: updatedUser._id.toString()
-         
+                id: updatedUser._id.toString(),
+            },
+        };
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User updated successfully');
-   }
-        const payload                 // Task 7: create JWT authentication using secret key from .env file
-        res.json(
-        logger.error(error);{ authtoken });
-    } catch (e) {
-        return res.status(500).send('Internal server error');
+        res.json({ authtoken });
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).send("Internal Server Error");
     }
 })
 
