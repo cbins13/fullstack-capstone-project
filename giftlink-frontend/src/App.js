dbd26d6a65852c85f4cecfa2961eb4e95f12ec36
Christian Binay-an <christianbinayan3@gmail.com>
2026-01-06 10:58:31 -0500

    Commit implement backend in profile component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage'
+import Profile from './components/Profile/Profile';
 
 function App() {
 
@@ -19,9 +20,10 @@ function App() {
                 <Route path="/" element={<MainPage />} />
                 <Route path="/app" element={<MainPage />} />
                 <Route path="/app/product/:productId" element={<DetailsPage />} />
-                <Route path="/app/search" element={<SearchPage/>}/>
-                <Route path="/app/login" element={<LoginPage/>}/>
-                <Route path="/app/register" element={<RegisterPage/>}/>
+                <Route path="/app/search" element={<SearchPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/profile" element={<Profile />} />
             </Routes>
         </>
     );
