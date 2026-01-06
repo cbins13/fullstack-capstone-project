a18fb48b9d1d8133be8f0043c53248ff11457b0a
Christian Binay-an <christianbinayan3@gmail.com>
2026-01-06 01:53:15 -0500

    implement details page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,19 +4,21 @@ import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
 
-  return (
-    <>
-        <Navbar/>
-        <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-        </Routes>
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                {/* the final code will not pass the products to every page, but each page will call the server API */}
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
+            </Routes>
         </>
-  );
+    );
 }
 
 export default App;
